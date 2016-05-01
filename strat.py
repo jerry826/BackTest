d8243c5dc682e97882795ac9f53a8d98de63aa7c
Jerry Lv <jiayang826@qq.com>
2016-05-01 22:51:56 +0800

    修改

--- strat.py
+++ strat.py
@@ -1,1 +1,23 @@
-__author__ = 'Jerry'
+# -*- encoding=utf8 -*-
+"""
+@author: Jerry
+@contact: lvjy3.15@sem.tsinghua.edu.com
+@file: strat.py
+@time: 2016/5/1 22:29
+"""
+
+
+def MA(df):
+	pass
+
+
+def MACD(df):
+	pass
+
+
+def id(df):
+	pass
+
+
+if __name__ == '__main__':
+	pass
